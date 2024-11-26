ae598cc387e1f75949e65e426ffafd806b6ed5ab
pmhanh <150933678+pmhanh@users.noreply.github.com>
2024-11-26 15:46:36 +0700

    change index.js

--- index.js
+++ index.js
@@ -7,22 +7,6 @@ const port = 3000;
 const expressHbs = require("express-handlebars");
 const {createPagination} = require("express-handlebars-paginate");
 app.use(express.static(__dirname + "/html"));
-const { Pool } = require("pg");
-const pool = new Pool({
-  user: 'postgres.dtxdgywsvylixjnnermi', // Replace with your actual PostgreSQL user
-  host: 'aws-0-ap-southeast-1.pooler.supabase.com', // Replace with your Supabase URL
-  database: 'postgres', // Replace with your actual database name
-  password: 'phammyhanh154@', // Replace with your password
-  port: 6543,
-});
-
-pool.query('SELECT NOW()', (err, res) => {
-  if (err) {
-    console.error('Database connection error', err.stack);
-  } else {
-    console.log('Database connected:', res.rows[0]);
-  }
-});
 
 app.engine(
   "hbs",
@@ -49,7 +33,7 @@ app.engine(
 app.set("view engine", "hbs");
 
 app.get("/", (req, res) => res.redirect("/blogs")  )
-  
+app.set("views", __dirname + "/views");
 app.use("/blogs", require("./routes/blogRouter"));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
